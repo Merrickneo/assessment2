71342a82e77e17764c0466394176f2cc2f507bcf
Merrick <merrickneo@gmail.com>
2024-08-28 18:15:00 +0800

    Update main.tf to create function URL

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   region = "ap-southeast-1" 
 }
 
-resource "aws_lambda_function" "example" {
+resource "aws_lambda_function" "lambda" {
   function_name = "return_details"
   runtime        = "python3.12"
   role           = data.aws_iam_role.existing_role.arn
@@ -12,6 +12,16 @@ resource "aws_lambda_function" "example" {
   source_code_hash = filebase64sha256("lambda_function.zip")
   filename         = "lambda_function.zip"
   publish          = true
+}
+
+# Create a Lambda function URL
+resource "aws_lambda_function_url" "example_url" {
+  function_name = aws_lambda_function.lambda.function_name
+  authorization_type = "NONE"  # You can set this to "AWS_IAM" if you need to restrict access
+
+  cors {
+    allow_origins = ["*"]  # Adjust as needed
+  }
 }
 
 # Reference to the existing IAM role
